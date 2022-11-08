be24d1a83c066d4187501d572fd340f9dbce2b83
raymundo pena <raymon389@gmail.com>
2022-11-08 09:36:21 -0500

    Dom munipulation Olympics

--- 3-dom-manipulation-olympics/dom-olympics-start/index.js
+++ 3-dom-manipulation-olympics/dom-olympics-start/index.js
@@ -2,12 +2,26 @@
 The HTML for the header's container exists but the header and the name don't exist yet.
 Feel free to change `.style` or use `.name` or `.header` classes from the CSS file."
 */
+// header
 const header = document.getElementById("header")
 header.textContent = "JavaScript Made This!!"
 header.style.textAlign = "center"
-header.style.fontSize = "25px"
+const myName = document.createElement("span")
+myName.textContent = "[Raymundo Pena]"
+myName.style.textAlign = "center"
+myName.style.color = "orange"
+const wrote = document.createElement("span").textContent = "wrote the JavaScript"
+const name = document.getElementById("name").append(myName, wrote)
 
-const div = document.createElement
-const ame = document.createElement("span").textContent = "[Raymundo Pena]"
-console.log(ame)
-header.append(ame)
+// chat box
+const msgs = document.querySelectorAll(".messages > div")
+for(i = 0; i < msgs.length; i++){
+    msgs[i].textContent = "something fun and good."
+}
+
+// clear box
+const clear = document.getElementById("clear-button")
+let box = document.getElementById("input")
+clear.addEventListener("click", function(){
+    box.value = ""
+})
