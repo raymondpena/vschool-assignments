a3aaa4515042452b3f435b79e36ec139458e2390
raymundo pena <raymon389@gmail.com>
2022-11-13 10:30:57 -0500

    moms shopping list extra credit update

--- 5-moms-shoppping-list/moms-shopping-list-start/index.js
+++ 5-moms-shoppping-list/moms-shopping-list-start/index.js
@@ -12,28 +12,62 @@ function addeventlisteners(){
 }
 addeventlisteners()
 //New item
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
     const listItem = document.createElement("li")
     const item = document.createElement("div")
     const itemText = document.createElement("p").textContent = this.title.value
     const editButton = document.createElement("button")
     const xButton = document.createElement("button")
-    xButton.setAttribute("class", "delete")
-    xButton.textContent = "X"
     item.append(itemText)
     editButton.textContent = "edit"
     listItem.append(item, editButton, xButton)
     list.prepend(listItem)
+    xButton.setAttribute("class", "delete")
+    xButton.textContent = "X"
     xButton.addEventListener("click", (e) =>{
         e.target.parentElement.remove()
     })
     this.title.value = ""
 })
 
+// Extra Credit
 
+const editButton = document.getElementsByClassName("edit")
+const inputBox = document.createElement("input")
+function addinputbox(){
+    for(i = 0; i < editButton.length; i++){
+        editButton[i].addEventListener("click", (e) => {
+            e.preventDefault()
+            e.target.parentElement.append(inputBox)
+            inputBox.classList.add("inputbox")
+            e.target.textContent = "save"
+            function removeinputbox(){
+                for(i = 0; i < editButton.length; i++){
+                    editButton[i].addEventListener("click", (e) => {
+                        e.preventDefault()
+                        e.target.parentElement.getElementsByClassName("inputbox").remove()
+                        e.target.textContent = "edit"
+                    })
+                }
+            }
+            removeinputbox()    
+        })
+    }
+}
+addinputbox()
 
-//Nodelist testbox
+function removeinputbox(){
+    for(i = 0; i < editButton.length; i++){
+        editButton[i].addEventListener("click", (e) => {
+            e.preventDefault()
+            e.target.parentElement.getElementsByClassName("inputbox").remove()
+            e.target.textContent = "edit"
+        })
+    }
+}
+
+//testbox
 var test = document.getElementsByClassName("delete")
 const testButton = document.createElement("button")
 testButton.style.width = "30px"
@@ -42,5 +76,5 @@ testButton.style.border = "solid black"
 document.body.append(testButton)
 
 testButton.addEventListener("click", () => {
-    console.log(document.getElementsByClassName("delete"))
+    console.log(document.querySelectorAll("li"))
 })
